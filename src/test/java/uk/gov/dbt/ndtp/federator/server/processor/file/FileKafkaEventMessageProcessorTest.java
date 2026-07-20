e4df3dadfa83cd31fb0f3f75acfab3f8c67dbaee
Ethan Higgins <ethan.higgins@informed.com>
2026-07-20 17:23:50 +0100

    bugfix: ensure consistent propertyutil teardown

--- src/test/java/uk/gov/dbt/ndtp/federator/server/processor/file/FileKafkaEventMessageProcessorTest.java
+++ src/test/java/uk/gov/dbt/ndtp/federator/server/processor/file/FileKafkaEventMessageProcessorTest.java
@@ -12,6 +12,7 @@ import static org.mockito.Mockito.*;
 import java.lang.reflect.Field;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.dbt.ndtp.federator.common.model.FileTransferRequest;
 import uk.gov.dbt.ndtp.federator.common.model.SourceType;
@@ -48,6 +49,11 @@ class FileKafkaEventMessageProcessorTest {
         Field validatorField = FileKafkaEventMessageProcessor.class.getDeclaredField("validator");
         validatorField.setAccessible(true);
         validatorField.set(processor, mockValidator);
+    }
+
+    @AfterEach
+    void tearDown() {
+        PropertyUtil.clear();
     }
 
     @Test
