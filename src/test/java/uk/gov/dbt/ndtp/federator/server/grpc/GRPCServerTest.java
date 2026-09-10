ce35b97908b47f9ae7ca45674d6c103871757eb8
Lord-Sam98 <sam.lord@informed.com>
2026-09-10 16:13:29 +0100

    feat(dpav-3023): javadoc and licensing agreement

--- src/test/java/uk/gov/dbt/ndtp/federator/server/grpc/GRPCServerTest.java
+++ src/test/java/uk/gov/dbt/ndtp/federator/server/grpc/GRPCServerTest.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme and is legally
+ * attributed to the Department for Business and Trade (UK) as the governing entity.
+ */
 package uk.gov.dbt.ndtp.federator.server.grpc;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
