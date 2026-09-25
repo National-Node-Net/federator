0039cd1f9da4fa5eea1497c21c1c94614cd61108
Syed Awsaf <syed.awsaf@informed.com>
2026-09-25 19:19:54 +0100

    style(OSPO): reflow license header comments to satisfy spotless line length

--- src/main/java/uk/gov/dbt/ndtp/federator/server/processor/file/FileChunkStreamer.java
+++ src/main/java/uk/gov/dbt/ndtp/federator/server/processor/file/FileChunkStreamer.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme
-// and is legally attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing entity.
+// and is legally attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing
+// entity.
 
 package uk.gov.dbt.ndtp.federator.server.processor.file;
 
--- src/test/java/uk/gov/dbt/ndtp/federator/client/lifecycle/AutoClosableShutdownTaskTest.java
+++ src/test/java/uk/gov/dbt/ndtp/federator/client/lifecycle/AutoClosableShutdownTaskTest.java
@@ -2,7 +2,8 @@
 // /home/vagrant/development/workspace/federator/src/test/java/uk/gov/dbt/ndtp/federator/client/lifecycle/AutoClosableShutdownTaskTest.java
 // SPDX-License-Identifier: Apache-2.0
 // © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme
-// and is legally attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing entity.
+// and is legally attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing
+// entity.
 
 package uk.gov.dbt.ndtp.federator.client.lifecycle;
 
